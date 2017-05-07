10b3650b14e6da2119f19453173a301c35c8ab41
murayama709 <murayama709@gmail.com>
2017-05-07 20:43:43 +0900

    add lookup util

--- nagare-func/src/test/java/com/nagare/util/LookupperExamples.java
+++ nagare-func/src/test/java/com/nagare/util/LookupperExamples.java
@@ -1,14 +1,13 @@
 package com.nagare.util;
 
 import com.nagare.parts.Color;
-import com.nagare.util.Lookupper;
 import org.junit.Test;
 
 /**
  * @author ken.murayama
  *
  */
-public class LookupperTest {
+public class LookupperExamples {
     @Test
     public void test() {
         Color color = Color.lookup("青");
@@ -16,6 +15,5 @@ public class LookupperTest {
 
         color = Lookupper.lookup(Color.values(), "赤");
         System.out.println(color);
-
     }
 }
